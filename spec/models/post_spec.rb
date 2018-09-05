d09b98e1c88f62b41dd41daa4685e28e34923caf
Ryan Manchester <ryanm17@gmail.com>
2018-09-05 20:31:56 +0000

    automatically backed up by learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can be created' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post).to be_valid
+  end
 end
